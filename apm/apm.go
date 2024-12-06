dba88b64b5d1f1e6c8e6cf3ee59d60f9a962cbc1
hedon954 <171725713@qq.com>
2024-12-06 12:57:42 +0800

    feat(apm): support custom headers for grpc client to otel exporter

--- apm/apm.go
+++ apm/apm.go
@@ -17,9 +17,17 @@ import (
 )
 
 type apmBuilder struct {
-	res       *resource.Resource
+	// res is the resource for the apm, if not set, a default resource will be created.
+	res *resource.Resource
+
+	// grpcToken is the grpc auth token for the apm, it is optional.
 	grpcToken string
-	sampler   sdktrace.Sampler
+
+	// sampler is the custom sampler for the apm, it is optional.
+	sampler sdktrace.Sampler
+
+	// headers for the grpc client to otel exporter, it is optional.
+	headers map[string]string
 }
 
 // ApmOption is the option for the apm.
@@ -46,11 +54,22 @@ func WithSampler(sampler sdktrace.Sampler) ApmOption {
 	}
 }
 
+// WithGrpcHeader sets the headers for the grpc client to otel exporter, it is optional.
+func WithGrpcHeader(headers map[string]string) ApmOption {
+	return func(b *apmBuilder) {
+		for k, v := range headers {
+			b.headers[k] = v
+		}
+	}
+}
+
 // NewAPM creates a new APM component, which is a wrapper of opentelemetry.
 func NewAPM(otelEndpoint string, opts ...ApmOption) (closeFunc func(), err error) {
 	ctx := context.Background()
 
-	b := &apmBuilder{}
+	b := &apmBuilder{
+		headers: make(map[string]string),
+	}
 	for _, opt := range opts {
 		opt(b)
 	}
@@ -75,15 +94,18 @@ func NewAPM(otelEndpoint string, opts ...ApmOption) (closeFunc func(), err error
 		b.res = res
 	}
 
+	// setup auth header
+	if b.grpcToken != "" {
+		b.headers["Authorization"] = b.grpcToken
+	}
+
 	// setup a trace exporter
 	ctx, cancel := context.WithTimeout(ctx, time.Second*10)
 	defer cancel()
 	traceExporter, err := otlptracegrpc.New(ctx,
 		otlptracegrpc.WithInsecure(),
 		otlptracegrpc.WithEndpoint(otelEndpoint),
-		otlptracegrpc.WithHeaders(map[string]string{
-			"Authentication": b.grpcToken,
-		}),
+		otlptracegrpc.WithHeaders(b.headers),
 		otlptracegrpc.WithCompressor(gzip.Name),
 	)
 	if err != nil {
